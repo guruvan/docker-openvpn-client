a173deb5517d134e4b83e6ec6afaf80bf0edc8f4
Rob Nelson <guruvan@maza.club>
2015-03-16 00:52:55 +0000

    add checks for resolv.conf symlink in host-startup script

--- host-startup/run-openvpn-client-cli.sh
+++ host-startup/run-openvpn-client-cli.sh
@@ -1,13 +1,38 @@
 #!/bin/bash
-cp -av /etc/resolv.conf ./resolv-conf.host
+############################################
+### This provided as an example
+### To properly copy your resolv.conf before
+### and after connection to VPN, we run 
+### the docker container in the foreground
+### 
+### If you prefer to put the container in the 
+### background, with "docker run -d" then 
+### you'll need a way to manage your hosts resolv.conf
+### or use other means. 
+
+cat  /etc/resolv.conf > ./resolv-conf.host
+if [  -h /etc/resolv.conf ]
+then
+  rm /etc/resolv.conf
+  sudo cp ./resolv-conf.host /etc/resolv.conf
+  export SYMRESOLV=true
+fi
 DOCKERBIN=$(which docker)
 ${DOCKERBIN} pull mazaclub/openvpn-client
-${DOCKERBIN} run -d \
+${DOCKERBIN} run \
  --privileged \
  --rm --net host \
  --name openvpn_client \
  -v /etc/resolv.conf:/etc/resolv.conf \
  -v $(pwd)/ovpn:/etc/openvpn \
  mazaclub/openvpn-client
+
 set -x
-sudo cp -av ./resolv-conf.host /etc/resolv.conf
+
+if [ "$SYMRESOLV" = "true" ]
+then 
+  rm /etc/resolv.conf
+  ln -s /run/resolvconf/resolv.conf /etc/resolv.conf
+else
+  sudo cp -av ./resolv-conf.host /etc/resolv.conf
+fi
